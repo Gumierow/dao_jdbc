6b7359acc5da1c484a81494cdca0cbb48bf16f48
Gumierow <guilherme.gumiero@outlook.com>
2024-05-03 17:18:28 -0300

    DepartmentDaoJDBC insert

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -15,12 +15,16 @@ public class Program2 {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
 		System.out.println("=== TEST 1: findById =======");
-		Department dep = departmentDao.findById(1);
-		System.out.println(dep);
+		Department dep1 = departmentDao.findById(1);
+		System.out.println(dep1);
 
 		System.out.println("=== TEST 2: findAll =======");
-		
 		System.out.println(departmentDao.findAll());
+		
+		System.out.println("=== TEST 3: insert =======");
+		Department dep2 = new Department(null, "Music");
+		departmentDao.insert(dep2);
+		System.out.println("Insertion complete! Department inserted: " + dep2);
 	}
 
 }
